e0ab3fb598844b687615f72c918f3ac5285150e0
nelDaryl <neldaryl28@gmail.com>
2025-10-31 16:40:53 +0100

    Add files via upload

---
+++ Exo2.R
@@ -0,0 +1,25 @@
+# Création du tableau de contingence
+boisson <- matrix(c(30, 10, 10,
+                    20, 25, 5),
+                  nrow = 2,
+                  byrow = TRUE)
+
+# Nommer les lignes et les colonnes
+rownames(boisson) <- c("Homme", "Femme")
+colnames(boisson) <- c("Café", "Thé", "Jus")
+
+# Afficher le tableau
+print(boisson)
+
+# Réaliser le test du Khi-deux d'indépendance
+test_chi2 <- chisq.test(boisson)
+
+# Afficher les résultats du test
+print(test_chi2)
+
+# Si tu veux afficher les effectifs attendus :
+print(test_chi2$expected)
+
+# Si tu veux afficher la p-valeur seule :
+print(test_chi2$p.value)
+
